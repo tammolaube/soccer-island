3b6cc0795963a0231f882dbd46b31c75b3cac72c
Tammo Laube <tammolaube@gmail.com>
2014-10-18 12:30:04 -1000

    added club and many to many relationships for plays for and coach for . also changed hawaii address to address

--- soccer_island/soccer_island/gamestats/models.py
+++ soccer_island/soccer_island/gamestats/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
-class HawaiiAddress(models.Model):
+class Address(models.Model):
     street = models.CharField(max_length=100)
     city = models.CharField(max_length=50, default='Honolulu')
+    state = models.CharField(max_length=2, default='HI')
     zip_code = models.CharField(max_length=20, default='968')
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
 
 class Person(models.Model):
     first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50)
     birthday = models.DateField()
     GENDER_CHOICES = (
@@ -15,21 +18,32 @@ class Person(models.Model):
             ('F', 'Female'),
         )
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=False, default='M')
-    address = models.ForeignKey(HawaiiAddress, blank=True, null=True)
+    address = models.ForeignKey(Address, blank=True, null=True)
 
 class Player(models.Model):
     person = models.ForeignKey(Person)
+    registered = models.ManyToManyField(Team, through='PlaysFor', through_fields=('player', 'team'))
+
+class PlayFor(models.Model):
+    player = models.ForeignKey(Player)
+    team = models.ForeignKey(Team)
+    from_date = models.DateField()
+    to_date = models.DateField(blank=True, null=True)
 
 class Coach(models.Model):
-    phone = models.CharField(max_length=20, blank=True)
-    email = models.EmailField()
+   person = models.ForeignKey(Person)
+
+class CoachFor(models.Model):
+    coach = models.ForeignKey(Coach)
+    team = models.ForeignKey(Team)
+    from_date = models.DateField()
+    to_date = models.DateField(blank=True, null=True)
     RESPONSIBILITY_CHOICES = (
             ('C', 'Coach'),
             ('M', 'Manager'),
             ('A', 'Assistant Coach'),
         )
     responsiblity = models.CharField(max_length=1, choices=RESPONSIBILITY_CHOICES, null=False, default='C')
-    person = models.ForeignKey(Person)
 
 class Referee(models.Model)
     person = models.ForeignKey(Person)
@@ -66,3 +80,9 @@ class Team(models.Model):
             ),
         )
     classification = models.CharField(max_length=5, choices=CLASSIFICATION_CHOICES)
+
+class Club(models.Model):
+    name = models.CharField(max_length=50)
+    short_name = models.CharField(max_length=3)
+    logo = models.ImageField()
+    address = models.ForeignKey(Address, blank=True, null=True)
