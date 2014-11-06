696ea6e12cf8c8a5b0b44ec71b948a0cbc77301a
root <tammolaube@gmail.com>
2014-11-06 02:51:49 -0500

    fixed error in view

--- soccer_island/stats/views.py
+++ soccer_island/stats/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-from stats.models import Team
+from stats.models import Classification, Competition, Season, Team
 
 def home(request):
     template = loader.get_template('stats/home.html')
@@ -10,12 +10,10 @@ def home(request):
     return HttpResponse(template.render(context))
 
 def season_overview(request, classification, competition, season):
-    teams = Team.objects.filter(
-        season__slug=season,
-        season__competition__slug=competition,
-        season__competition__classifcation__slug=classification
-    )
-
+    classifications_qs = Classification.objects.filter(slug=classification)
+    competitions_qs = Competition.objects.filter(classification__in=classifications_qs, slug=competition)
+    seasons_qs = Season.objects.filter(competition__in=competitions_qs, slug=season)
+    teams = Team.objects.filter(season__in=seasons_qs)
     template = loader.get_template('stats/season_overview.html')
     context = RequestContext(request, {
         'classification': classification,
